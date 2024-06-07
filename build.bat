bbe994c093f565dfdb4f1e084f60803de563ccaf
Artem Yudin <arrannis1800@gmail.com>
2024-06-07 21:31:49 +0400

    startup script

---
+++ build.bat
@@ -0,0 +1,3 @@
+g++ -Wall -Wextra -Wpedantic main.cpp level.cpp ball.cpp renderer.cpp -IC:\\SDL2\\include -LC:\\SDL2\\lib\\x64 -o app -IC:\\SDL2\\include -LC:\\SDL2\\lib\\x64 -lSDL2main -lSDL2 -std=c++20
+
+call app.exe
